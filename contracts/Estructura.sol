3ff5370eb202036076197f1a8b44ca789441261a
"Bermejo Catalan" <"bercawork@gmail.com">
2022-02-09 13:38:39 +0100

    Estructura y Variables

---
+++ contracts/Estructura.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.O
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Estructura {
+
+    constructor() {
+
+    }
+}
---
+++ contracts/Variables.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.O
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Estructura {
+
+    int cantidad;
+    uint cantidadSinSigno;
+    address direccion;
+    bool firmado;
+    
+
+
+    constructor(bool estaFirmado) {
+        direccion = msg.sender;
+        firmado = estaFirmado;
+
+    }
+}
